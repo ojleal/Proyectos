27ff9e77979f3b9bc154a14585a2c7d2d00cea7f
ojleal <35660770+ojleal@users.noreply.github.com>
2020-03-11 14:36:41 -0400

    Add files via upload

---
+++ init-firebase.js
@@ -0,0 +1,16 @@
+<!-- TODO: Add SDKs for Firebase products that you want to use
+https://firebase.google.com/docs/web/setup#config-web-app -->
+<script>
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyB0PstTPCl4Ti7FwhIheJV8zwIaeNEkpI8",
+    authDomain: "land-251714.firebaseapp.com",
+    databaseURL: "https://land-251714.firebaseio.com",
+    projectId: "land-251714",
+    storageBucket: "",
+    messagingSenderId: "746522438034",
+    appId: "1:746522438034:web:116895e70766400c80b94b"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+</script>
